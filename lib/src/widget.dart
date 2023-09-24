122dc918b7e35acbe79e9885aacdbcefa61f9f16
Mateus Felipe C. C. Pinto <mateusfccp@gmail.com>
2023-09-24 10:33:47 -0300

    Version 2.0

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
-import 'element.dart';
-import 'constraint_type.dart';
-import 'rendering.dart';
-
 /// A widget that imposes additional constraints on its child, based on a [prototype].
 ///
 /// For example, if you wanted [child] to be constrained to the exact size of a text,
@@ -20,94 +16,397 @@ import 'rendering.dart';
 /// See also:
 ///
 ///  * [ConstrainedBox](https://api.flutter.dev/flutter/widgets/ConstrainedBox-class.html), the equivalent class that receives a [BoxConstraints](https://api.flutter.dev/flutter/rendering/BoxConstraints-class.html) instead of a [prototype].
-class PrototypeConstrainedBox extends RenderObjectWidget {
+final class PrototypeConstrainedBox extends RenderObjectWidget {
   /// Constrains the given [child] to have the exact same size as [prototype].
   const PrototypeConstrainedBox.tight({
     super.key,
     required this.prototype,
     required this.child,
-  })  : _horizontalConstraint = ConstraintType.tight,
-        _verticalConstraint = ConstraintType.tight;
+  })  : constrainMinWidth = true,
+        constrainMaxWidth = true,
+        constrainMinHeight = true,
+        constrainMaxHeight = true;
 
-  /// Constrains the given [child] to have the exact same width as [prototype].
-  const PrototypeConstrainedBox.tightHorizontal({
+  /// Constrains the given [child] to have the exact same size as [prototype].
+  const PrototypeConstrainedBox.tightFor({
     super.key,
+    bool width = false,
+    bool height = false,
     required this.prototype,
     required this.child,
-  })  : _horizontalConstraint = ConstraintType.tight,
-        _verticalConstraint = ConstraintType.unconstrained;
-
-  /// Constrains the given [child] to have the exact same height as [prototype].
-  const PrototypeConstrainedBox.tightVertical({
-    super.key,
-    required this.prototype,
-    required this.child,
-  })  : _horizontalConstraint = ConstraintType.unconstrained,
-        _verticalConstraint = ConstraintType.tight;
+  })  : constrainMinWidth = width,
+        constrainMaxWidth = width,
+        constrainMinHeight = height,
+        constrainMaxHeight = height;
 
   /// Constrains the given [child] to forbid it to be larger than [prototype].
   const PrototypeConstrainedBox.loose({
     super.key,
     required this.prototype,
     required this.child,
-  })  : _horizontalConstraint = ConstraintType.loose,
-        _verticalConstraint = ConstraintType.loose;
+  })  : constrainMinWidth = false,
+        constrainMaxWidth = true,
+        constrainMinHeight = false,
+        constrainMaxHeight = true;
 
-  /// Constrains the given [child] to forbid it to be wider than [prototype].
-  const PrototypeConstrainedBox.looseHorizontal({
+  /// Creates a widget that imposes the [prototype] constraints on its [child].
+  ///
+  /// The [prototype] widget is used as a reference for applying constraints to
+  /// the [child].
+  ///
+  /// The [horizontalConstraint] and [verticalConstraint] parameters allow you
+  /// to specify the constraint type for each axis independently.
+  const PrototypeConstrainedBox({
     super.key,
+    required this.constrainMinWidth,
+    required this.constrainMaxWidth,
+    required this.constrainMinHeight,
+    required this.constrainMaxHeight,
     required this.prototype,
     required this.child,
-  })  : _horizontalConstraint = ConstraintType.loose,
-        _verticalConstraint = ConstraintType.unconstrained;
-
-  /// Constrains the given [child] to forbid it to be taller than [prototype].
-  const PrototypeConstrainedBox.looseVertical({
-    super.key,
-    required this.prototype,
-    required this.child,
-  })  : _horizontalConstraint = ConstraintType.unconstrained,
-        _verticalConstraint = ConstraintType.loose;
+  });
 
   /// The widget whose constraints are going to be imposed in [child].
   ///
-  /// This widget pass through the layout phase, as it needs to be laid out for we to know it's constraints.
-  /// However, it does not pass through the paint phase, and is, thus, not visible in any way.
+  /// This widget is never laid out, as we use [RenderBox.computeDryLayout] to
+  /// compute its size.
+  ///
+  /// This widget is never painted.
+  ///
+  /// The semantics of this widget are not included in the semantics tree.
   final Widget prototype;
 
   /// The widget below this widget in the tree.
   final Widget? child;
 
-  final ConstraintType _horizontalConstraint;
-  final ConstraintType _verticalConstraint;
+  final bool constrainMinWidth;
+  final bool constrainMaxWidth;
+  final bool constrainMinHeight;
+  final bool constrainMaxHeight;
 
   @override
   RenderObject createRenderObject(BuildContext context) {
     return RenderPrototypeConstrainedBox(
-      _horizontalConstraint,
-      _verticalConstraint,
+      constrainMinWidth,
+      constrainMaxWidth,
+      constrainMinHeight,
+      constrainMaxHeight,
     );
   }
 
   @override
   void updateRenderObject(BuildContext context, RenderPrototypeConstrainedBox renderObject) {
-    renderObject.horizontalConstraint = _horizontalConstraint;
-    renderObject.verticalConstraint = _verticalConstraint;
+    renderObject.constrainMinWidth = constrainMinWidth;
+    renderObject.constrainMaxWidth = constrainMaxWidth;
+    renderObject.constrainMinHeight = constrainMinHeight;
+    renderObject.constrainMaxHeight = constrainMaxHeight;
   }
+
+  @override
+  RenderObjectElement createElement() => _PrototypeConstrainedBoxElement(this);
 
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(DiagnosticsProperty<Widget>('prototype', prototype));
     properties.add(DiagnosticsProperty<Widget>('child', child));
-    properties.add(
-      DiagnosticsProperty<ConstraintType>('horizontal constraint', _horizontalConstraint),
-    );
-    properties.add(
-      DiagnosticsProperty<ConstraintType>('vertical constraint', _verticalConstraint),
+    properties.add(FlagProperty('constrainMinWidth', value: constrainMinWidth));
+    properties.add(FlagProperty('constrainMaxWidth', value: constrainMaxWidth));
+    properties.add(FlagProperty('constrainMinHeight', value: constrainMinHeight));
+    properties.add(FlagProperty('constrainMaxHeight', value: constrainMaxHeight));
+  }
+}
+
+final class RenderPrototypeConstrainedBox extends RenderProxyBox {
+  RenderPrototypeConstrainedBox(
+    this._constrainMinWidth,
+    this._constrainMaxWidth,
+    this._constrainMinHeight,
+    this._constrainMaxHeight,
+  );
+
+  bool get constrainMinWidth => _constrainMinWidth;
+  bool _constrainMinWidth;
+
+  set constrainMinWidth(bool value) {
+    if (_constrainMinWidth != value) {
+      _constrainMinWidth = value;
+      markNeedsLayout();
+    }
+  }
+
+  bool get constrainMaxWidth => _constrainMaxWidth;
+  bool _constrainMaxWidth;
+
+  set constrainMaxWidth(bool value) {
+    if (_constrainMaxWidth != value) {
+      _constrainMaxWidth = value;
+      markNeedsLayout();
+    }
+  }
+
+  bool get constrainMinHeight => _constrainMinHeight;
+  bool _constrainMinHeight;
+
+  set constrainMinHeight(bool value) {
+    if (_constrainMinHeight != value) {
+      _constrainMinHeight = value;
+      markNeedsLayout();
+    }
+  }
+
+  bool get constrainMaxHeight => _constrainMaxHeight;
+  bool _constrainMaxHeight;
+
+  set constrainMaxHeight(bool value) {
+    if (_constrainMaxHeight != value) {
+      _constrainMaxHeight = value;
+      markNeedsLayout();
+    }
+  }
+
+  RenderBox? _prototype;
+
+  RenderBox? get prototype => _prototype;
+
+  set prototype(RenderBox? value) {
+    if (_prototype != null) {
+      dropChild(_prototype!);
+    }
+    _prototype = value;
+    if (_prototype != null) {
+      adoptChild(_prototype!);
+    }
+    markNeedsLayout();
+  }
+
+  BoxConstraints? _prototypeConstraints;
+
+  void _computePrototypeConstraints() {
+    final size = prototype!.getDryLayout(constraints);
+
+    _prototypeConstraints = BoxConstraints(
+      minWidth: constrainMinWidth ? size.width : 0.0,
+      maxWidth: constrainMaxWidth ? size.width : double.infinity,
+      minHeight: constrainMinHeight ? size.height : 0.0,
+      maxHeight: constrainMaxHeight ? size.height : double.infinity,
     );
   }
 
   @override
-  RenderObjectElement createElement() => PrototypeConstrainedBoxElement(this);
+  double computeMinIntrinsicWidth(double height) {
+    final constraints = _prototypeConstraints!;
+    if (constraints.hasBoundedWidth && constraints.hasTightWidth) {
+      return constraints.minWidth;
+    } else {
+      final width = super.computeMinIntrinsicWidth(height);
+
+      assert(width.isFinite);
+
+      if (constraints.hasInfiniteWidth) {
+        return width;
+      } else {
+        return constraints.constrainWidth(width);
+      }
+    }
+  }
+
+  @override
+  double computeMaxIntrinsicWidth(double height) {
+    final constraints = _prototypeConstraints!;
+    if (constraints.hasBoundedWidth && constraints.hasTightWidth) {
+      return constraints.minWidth;
+    } else {
+      final width = super.computeMaxIntrinsicWidth(height);
+
+      assert(width.isFinite);
+
+      if (constraints.hasInfiniteWidth) {
+        return width;
+      } else {
+        return constraints.constrainWidth(width);
+      }
+    }
+  }
+
+  @override
+  double computeMinIntrinsicHeight(double width) {
+    final constraints = _prototypeConstraints!;
+    if (constraints.hasBoundedHeight && constraints.hasTightHeight) {
+      return constraints.minHeight;
+    } else {
+      final height = super.computeMinIntrinsicHeight(width);
+
+      assert(height.isFinite);
+
+      if (constraints.hasInfiniteHeight) {
+        return height;
+      } else {
+        return constraints.constrainWidth(height);
+      }
+    }
+  }
+
+  @override
+  double computeMaxIntrinsicHeight(double width) {
+    final constraints = _prototypeConstraints!;
+    if (constraints.hasBoundedHeight && constraints.hasTightHeight) {
+      return constraints.minHeight;
+    } else {
+      final height = super.computeMaxIntrinsicHeight(width);
+
+      assert(height.isFinite);
+
+      if (constraints.hasInfiniteHeight) {
+        return height;
+      } else {
+        return constraints.constrainWidth(height);
+      }
+    }
+  }
+
+  @override
+  void performLayout() {
+    _computePrototypeConstraints();
+
+    final constraints = _prototypeConstraints!;
+
+    if (child case final child?) {
+      child.layout(
+        constraints.enforce(this.constraints),
+        parentUsesSize: true,
+      );
+      size = child.size;
+    } else {
+      size = _prototypeConstraints!.enforce(constraints).constrain(Size.zero);
+    }
+  }
+
+  @override
+  Size computeDryLayout(BoxConstraints constraints) {
+    if (child case final child?) {
+      return child.getDryLayout(
+        _prototypeConstraints!.enforce(constraints),
+      );
+    } else {
+      return _prototypeConstraints!.enforce(constraints).constrain(Size.zero);
+    }
+  }
+
+  @override
+  void attach(PipelineOwner owner) {
+    super.attach(owner);
+    prototype?.attach(owner);
+  }
+
+  @override
+  void detach() {
+    super.detach();
+    prototype?.detach();
+  }
+
+  @override
+  void redepthChildren() {
+    super.redepthChildren();
+    if (prototype case final prototype?) {
+      redepthChild(prototype);
+    }
+  }
+
+  @override
+  void visitChildren(RenderObjectVisitor visitor) {
+    if (child case final child?) {
+      visitor(child);
+    }
+  }
+
+  @override
+  List<DiagnosticsNode> debugDescribeChildren() {
+    return [
+      ...super.debugDescribeChildren(),
+      if (prototype case final prototype?) prototype.toDiagnosticsNode(name: 'prototype'),
+    ];
+  }
+}
+
+final class _PrototypeConstrainedBoxElement extends RenderObjectElement {
+  _PrototypeConstrainedBoxElement(super.widget);
+
+  Element? _prototype;
+  Element? _child;
+
+  static final _prototypeSlot = Object();
+  static final _childSlot = Object();
+
+  @override
+  void visitChildren(ElementVisitor visitor) {
+    final prototype = _prototype;
+    if (prototype != null) {
+      visitor(prototype);
+    }
+
+    final child = _child;
+    if (child != null) {
+      visitor(child);
+    }
+  }
+
+  @override
+  void forgetChild(Element child) {
+    if (child == _child) {
+      _child = null;
+    } else if (child == _prototype) {
+      _prototype = null;
+    } else {
+      assert(false);
+    }
+    super.forgetChild(child);
+  }
+
+  @override
+  void mount(Element? parent, Object? newSlot) {
+    super.mount(parent, newSlot);
+    final widget = this.widget as PrototypeConstrainedBox;
+    _prototype = updateChild(_prototype, widget.prototype, _prototypeSlot);
+    _child = updateChild(_child, widget.child, _childSlot);
+  }
+
+  @override
+  void update(RenderObjectWidget newWidget) {
+    super.update(newWidget);
+    final widget = this.widget as PrototypeConstrainedBox;
+    assert(widget == newWidget);
+    _prototype = updateChild(_prototype, widget.prototype, _prototypeSlot);
+    _child = updateChild(_child, widget.child, _childSlot);
+  }
+
+  @override
+  void insertRenderObjectChild(RenderBox child, Object? slot) {
+    final renderObject = this.renderObject as RenderPrototypeConstrainedBox;
+    if (slot == _childSlot) {
+      renderObject.child = child;
+    } else if (slot == _prototypeSlot) {
+      renderObject.prototype = child;
+    }
+  }
+
+  @override
+  void moveRenderObjectChild(RenderObject child, Object? oldSlot, Object? newSlot) {
+    assert(false);
+  }
+
+  @override
+  void removeRenderObjectChild(RenderObject child, Object? slot) {
+    final renderObject = this.renderObject as RenderPrototypeConstrainedBox;
+    if (slot == _childSlot) {
+      assert(renderObject.child == child);
+      renderObject.child = null;
+    } else if (slot == _prototypeSlot) {
+      assert(renderObject.prototype == child);
+      renderObject.prototype = null;
+    } else {
+      assert(false);
+    }
+    assert(renderObject == this.renderObject);
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,272 +3,139 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:prototype_constrained_box/prototype_constrained_box.dart';
 
 void main() {
-  group(
-    'PrototypeConstrainedBox',
-    () {
-      group(
-        '.loose',
-        () {
-          testWidgets(
-            'should constrain the size of the child to the size of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.loose(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 128.0),
-                  ),
-                ),
-              );
+  group('PrototypeConstrainedBox', () {
+    group('.loose', () {
+      testWidgets('should prevent the child from being bigger than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.loose(
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 128.0),
+            );
+          },
+          expectation: const Size.square(64.0),
+        );
+      });
 
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
+      testWidgets('should allow the child from being smaller than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.loose(
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 32.0),
+            );
+          },
+          expectation: const Size.square(32.0),
+        );
+      });
+    });
 
-              expect(
-                renderBox.size,
-                const Size.square(64.0),
-              );
-            },
-          );
+    group('.tight', () {
+      testWidgets('should prevent the child from being bigger than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tight(
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 128.0),
+            );
+          },
+          expectation: const Size.square(64.0),
+        );
+      });
 
-          testWidgets(
-            'should allow the size of the child to be smaller than the size of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.loose(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 32.0),
-                  ),
-                ),
-              );
+      testWidgets('should prevent the child from being smaller than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tight(
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 32.0),
+            );
+          },
+          expectation: const Size.square(64.0),
+        );
+      });
+    });
 
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
+    group('.tightFor(width: true)', () {
+      testWidgets('should prevent the child from being wider than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tightFor(
+              width: true,
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 128.0),
+            );
+          },
+          expectation: const Size(64.0, 128.0),
+        );
+      });
 
-              expect(
-                renderBox.size,
-                const Size.square(32.0),
-              );
-            },
-          );
-        },
-      );
+      testWidgets('should prevent the child from being narrower than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tightFor(
+              width: true,
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 32.0),
+            );
+          },
+          expectation: const Size(64.0, 32.0),
+        );
+      });
+    });
 
-      testWidgets(
-        '.tight should force the child to have the same size as the prototype',
-        (WidgetTester tester) async {
-          final key = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tight(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key, dimension: 128.0),
-              ),
-            ),
-          );
+    group('.tightFor(height: true)', () {
+      testWidgets('should prevent the child from being taller than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tightFor(
+              height: true,
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 128.0),
+            );
+          },
+          expectation: const Size(128.0, 64.0),
+        );
+      });
 
-          final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
+      testWidgets('should prevent the child from being shorter than the prototype', (WidgetTester tester) async {
+        await _testWidget(
+          tester: tester,
+          builder: (key) {
+            return PrototypeConstrainedBox.tightFor(
+              height: true,
+              prototype: const SizedBox.square(dimension: 64.0),
+              child: SizedBox.square(key: key, dimension: 32.0),
+            );
+          },
+          expectation: const Size(32.0, 64.0),
+        );
+      });
+    });
+  });
+}
 
-          expect(
-            renderBox.size,
-            const Size.square(64.0),
-          );
+Future<void> _testWidget({
+  required WidgetTester tester,
+  required Widget Function(Key key) builder,
+  required Size expectation,
+}) async {
+  final key = UniqueKey();
+  await tester.pumpWidget(
+    Center(child: builder(key)),
+  );
 
-          final key2 = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tight(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key2, dimension: 32.0),
-              ),
-            ),
-          );
+  final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
 
-          final renderBox2 = tester.renderObject<RenderBox>(find.byKey(key2));
-
-          expect(
-            renderBox2.size,
-            const Size.square(64.0),
-          );
-        },
-      );
-
-      group(
-        '.looseHorizontal',
-        () {
-          testWidgets(
-            'should constrain only the width of the child to the width of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.looseHorizontal(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 128.0),
-                  ),
-                ),
-              );
-
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-              expect(
-                renderBox.size,
-                const Size(64.0, 128.0),
-              );
-            },
-          );
-
-          testWidgets(
-            'should allow the width of the child to be smaller than the width of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.looseHorizontal(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 32.0),
-                  ),
-                ),
-              );
-
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-              expect(
-                renderBox.size,
-                const Size(32.0, 32.0),
-              );
-            },
-          );
-        },
-      );
-
-      testWidgets(
-        '.tightHorizontal should force the child to have the same width as the prototype',
-        (WidgetTester tester) async {
-          final key = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tightHorizontal(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key, dimension: 128.0),
-              ),
-            ),
-          );
-
-          final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-          expect(
-            renderBox.size,
-            const Size(64.0, 128.0),
-          );
-
-          final key2 = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tightHorizontal(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key2, dimension: 32.0),
-              ),
-            ),
-          );
-
-          final renderBox2 = tester.renderObject<RenderBox>(find.byKey(key2));
-
-          expect(
-            renderBox2.size,
-            const Size(64.0, 32.0),
-          );
-        },
-      );
-
-      group(
-        '.looseVertical',
-        () {
-          testWidgets(
-            'should constrain only the height of the child to the height of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.looseVertical(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 128.0),
-                  ),
-                ),
-              );
-
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-              expect(
-                renderBox.size,
-                const Size(128.0, 64.0),
-              );
-            },
-          );
-
-          testWidgets(
-            'should allow the height of the child to be smaller than the height of the prototype',
-            (WidgetTester tester) async {
-              final key = UniqueKey();
-              await tester.pumpWidget(
-                Center(
-                  child: PrototypeConstrainedBox.looseVertical(
-                    prototype: const SizedBox.square(dimension: 64.0),
-                    child: SizedBox.square(key: key, dimension: 32.0),
-                  ),
-                ),
-              );
-
-              final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-              expect(
-                renderBox.size,
-                const Size(32.0, 32.0),
-              );
-            },
-          );
-        },
-      );
-
-      testWidgets(
-        '.tightVertical should force the child to have the same height as the prototype',
-        (WidgetTester tester) async {
-          final key = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tightVertical(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key, dimension: 128.0),
-              ),
-            ),
-          );
-
-          final renderBox = tester.renderObject<RenderBox>(find.byKey(key));
-
-          expect(
-            renderBox.size,
-            const Size(128.0, 64.0),
-          );
-
-          final key2 = UniqueKey();
-          await tester.pumpWidget(
-            Center(
-              child: PrototypeConstrainedBox.tightVertical(
-                prototype: const SizedBox.square(dimension: 64.0),
-                child: SizedBox.square(key: key2, dimension: 32.0),
-              ),
-            ),
-          );
-
-          final renderBox2 = tester.renderObject<RenderBox>(find.byKey(key2));
-
-          expect(
-            renderBox2.size,
-            const Size(32.0, 64.0),
-          );
-        },
-      );
-    },
+  expect(
+    renderBox.size,
+    expectation,
   );
 }
